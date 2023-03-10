8f080e84402ce78121dcd81ad175a402726df7e2
Andrew Todd <andrew.s.todd@wsu.edu>
2023-03-09 19:59:43 -0800

    Made some organizational changes. Moved blocks to an enum. Made a Chunk component.

--- src/world.rs
+++ src/world.rs
@@ -1,133 +1,58 @@
 use bevy::prelude::*;
 use rand::random;
-use noise::{NoiseFn, Perlin, Seedable, Fbm};
+use noise::{Perlin, Fbm};
 use noise::utils::{NoiseMapBuilder, PlaneMapBuilder, NoiseMap};
 
-#[derive(Component)]
-struct Identity(u32);
+#[derive(Component, Clone, Copy, PartialEq, Eq, Hash, Debug)]
+enum BlockType {
+    Grass,
+    Dirt,
+    Stone,
+}
 
-#[derive(Component)]
-struct Name(String);
-
-#[derive(Bundle)]
+#[derive(Bundle, Clone)]
 struct Block {
-    id: Identity,
-    name: Name,
+    block_type: BlockType,
     object: PbrBundle,
 }
 
-impl Block {
+#[derive(Component, Clone)]
+struct Chunk {
+    blocks: Vec<Block>,
+}
+
+impl Chunk {
     pub fn new(
-        block_type: &str,
-        coordinates: (i32, i32, i32),
+        start_pos: i32,
+        height_map: &NoiseMap,
         meshes: &mut ResMut<Assets<Mesh>>,
-        materials: &mut ResMut<Assets<StandardMaterial>>,)
-        -> Self {
-        match block_type {
-            "Grass" => {
-                Self {
-                    id: Identity(0),
-                    name: Name("Grass".to_string()),
-                    object: PbrBundle {
-                        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-                        material: materials.add(Color::rgb(0.1, 0.53, 0.0).into()),
-                        transform: Transform::from_translation(Vec3::new(
-                            coordinates.0 as f32, coordinates.1 as f32, coordinates.2 as f32,
-                        )),
-                        ..Default::default()
-                    },
-                }
-            }
-            "Dirt" => {
-                Self {
-                    id: Identity(1),
-                    name: Name("Dirt".to_string()),
-                    object: PbrBundle {
-                        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-                        material: materials.add(Color::rgb(1.0, 0.87, 0.71).into()),
-                        transform: Transform::from_translation(Vec3::new(
-                            coordinates.0 as f32, coordinates.1 as f32, coordinates.2 as f32,
-                        )),
-                        ..Default::default()
-                    },
-                }
-            }
-            _ => {
-                Self {
-                    id: Identity(2),
-                    name: Name("Stone".to_string()),
-                    object: PbrBundle {
-                        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-                        material: materials.add(Color::rgb(0.29, 0.36, 0.36).into()),
-                        transform: Transform::from_translation(Vec3::new(
-                            coordinates.0 as f32, coordinates.1 as f32, coordinates.2 as f32,
-                        )),
-                        ..Default::default()
-                    },
+        materials: &mut ResMut<Assets<StandardMaterial>>,) -> Self {
+        let mut blocks = Vec::new();
+
+        let SIZE = 16 + start_pos;
+
+        for x in -SIZE..SIZE {
+            for z in -SIZE..SIZE {
+                let noise = height_map.get_value(x as usize, z as usize);
+                let height = (noise * 10.0) as i32;
+                for y in 0..height {
+                    if y == height - 1 {
+                        let block = Block::new(BlockType::Grass, IVec3::new(x, y, z), meshes, materials);
+                        blocks.push(block);
+                    } else if y > height - 5 {
+                        let block = Block::new(BlockType::Dirt, IVec3::new(x, y, z), meshes, materials);
+                        blocks.push(block);
+                    } else {
+                        let block = Block::new(BlockType::Stone, IVec3::new(x, y, z), meshes, materials);
+                        blocks.push(block);
+                    }
                 }
             }
         }
-    }
-}
-
-fn chunk_gen(
-    start_pos: i32,
-    height_map: &NoiseMap,
-    meshes: &mut ResMut<Assets<Mesh>>,
-	materials: &mut ResMut<Assets<StandardMaterial>>,) -> Vec<Block> {
-
-    let mut blocks = Vec::new();
-
-    let SIZE = 16 + start_pos;
-
-    for x in -SIZE..SIZE {
-        for z in -SIZE..SIZE {
-            let noise = height_map.get_value(x as usize, z as usize);
-            let height = (noise * 10.0) as i32;
-            for y in 0..height {
-                if y == height - 1 {
-                    let block = Block::new("Grass", (x, y, z), meshes, materials);
-                    blocks.push(block);
-                } else if y > height - 5 {
-                    let block = Block::new("Dirt", (x, y, z), meshes, materials);
-                    blocks.push(block);
-                } else {
-                    let block = Block::new("Stone", (x, y, z), meshes, materials);
-                    blocks.push(block);
-                }
-            }
+        Self {
+            blocks,
         }
     }
-
-    blocks
-}
-
-pub fn world_gen(	
-    mut commands: Commands,
-	mut meshes: ResMut<Assets<Mesh>>,
-	mut materials: ResMut<Assets<StandardMaterial>>,) {
-        
-    let fbm = Fbm::<Perlin>::new(rand::random());
-
-    let height_map = PlaneMapBuilder::<_, 2>::new(&fbm)
-            .set_size(1000, 1000)
-            .set_x_bounds(-5.0, 5.0)
-            .set_y_bounds(-5.0, 5.0)
-            .build();
-
-    let mut chunks = Vec::new();
-
-    for i in 0..4 {
-        let chunk = chunk_gen(i * 16, &height_map, &mut meshes, &mut materials);
-        chunks.push(chunk);
-    }
-
-    for chunk in chunks {
-        commands.spawn_batch(chunk);
-    }
-}
-
-// NEED TO SAVE THE HEIGHT MAP TO A FILE SO THAT IT DOESN'T HAVE TO BE GENERATED EVERY TIME THE GAME IS STARTED
 
 // pub fn update_chunks(
 //     mut commands: Commands,
@@ -156,3 +81,77 @@ pub fn world_gen(
 //         }
 //     }
 // }
+}
+
+impl Block {
+    pub fn new(
+        block_type: BlockType,
+        coordinates: IVec3,
+        meshes: &mut ResMut<Assets<Mesh>>,
+        materials: &mut ResMut<Assets<StandardMaterial>>,)
+        -> Self {
+        match block_type {
+            BlockType::Grass => {
+                Self {
+                    block_type: BlockType::Grass,
+                    object: PbrBundle {
+                        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+                        material: materials.add(Color::rgb(0.1, 0.53, 0.0).into()),
+                        transform: Transform::from_translation(coordinates.as_vec3()),
+                        ..Default::default()
+                    },
+                }
+            }
+            BlockType::Dirt => {
+                Self {
+                    block_type: BlockType::Dirt,
+                    object: PbrBundle {
+                        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+                        material: materials.add(Color::rgb(1.0, 0.87, 0.71).into()),
+                        transform: Transform::from_translation(coordinates.as_vec3()),
+                        ..Default::default()
+                    },
+                }
+            }
+            _ => {
+                Self {
+                    block_type: BlockType::Stone,
+                    object: PbrBundle {
+                        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+                        material: materials.add(Color::rgb(0.29, 0.36, 0.36).into()),
+                        transform: Transform::from_translation(coordinates.as_vec3()),
+                        ..Default::default()
+                    },
+                }
+            }
+        }
+    }
+}
+
+pub fn world_gen(	
+    mut commands: Commands,
+	mut meshes: ResMut<Assets<Mesh>>,
+	mut materials: ResMut<Assets<StandardMaterial>>,) {
+        
+    let fbm = Fbm::<Perlin>::new(rand::random());
+
+    let height_map = PlaneMapBuilder::<_, 2>::new(&fbm)
+            .set_size(1000, 1000)
+            .set_x_bounds(-5.0, 5.0)
+            .set_y_bounds(-5.0, 5.0)
+            .build();
+
+    let mut chunks = Vec::new();
+
+    for i in 0..4 {
+        chunks.push(Chunk::new(i * 16, &height_map, &mut meshes, &mut materials));
+    }
+
+    for chunk in chunks.iter() {
+        for block in chunk.blocks.iter() {
+            commands.spawn(block.object.clone());
+        }
+    }
+}
+
+// NEED TO SAVE THE HEIGHT MAP TO A FILE SO THAT IT DOESN'T HAVE TO BE GENERATED EVERY TIME THE GAME IS STARTED
