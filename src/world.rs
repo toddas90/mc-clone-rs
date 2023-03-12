f39c160e22317a0781486e5459af0c2d3c5adf11
Andrew Todd <andrew.s.todd@wsu.edu>
2023-03-11 17:17:26 -0800

    Making some progress with raycasting.

--- src/world.rs
+++ src/world.rs
@@ -1,19 +1,18 @@
-use std::collections::hash_map::DefaultHasher;
 use std::hash::{Hash, Hasher};
+use fxhash::FxHasher;
 use std::sync::{Mutex, Arc};
-use bevy::prelude::*;
+use bevy::{prelude::*, transform};
 use bevy::utils::HashSet;
 use noise::{Perlin, Fbm};
 use noise::utils::{NoiseMapBuilder, PlaneMapBuilder};
 use rayon::prelude::*;
 
-const CHUNK_SIZE: i32 = 4;
-const VIEW_DISTANCE: i32 = 4;
-const GENERATION_RADIUS: i32 = 4;
-const CACHE_SIZE: i32 = 4;
+const CHUNK_SIZE: i32 = 16;
+const VIEW_DISTANCE: i32 = 2;
+const GENERATION_RADIUS: i32 = 2;
 const SEED : u32 = 69;
 
-#[derive(Component, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
+#[derive(Component, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord, Debug)]
 struct Position {
     x: i32,
     y: i32,
@@ -29,6 +28,7 @@ pub struct Block {
 pub struct Chunk {
     blocks: HashSet<Block>,
     center_pos: IVec2,
+    rendered: bool,
 }
 
 #[derive(Resource, Default)]
@@ -41,6 +41,14 @@ impl Position {
     fn new(x: i32, y: i32, z: i32) -> Self {
         Self { x, y, z }
     }
+
+    fn from_ivec3(pos: IVec3) -> Self {
+        Self {
+            x: pos.x,
+            y: pos.y,
+            z: pos.z,
+        }
+    }
 }
 
 impl Block {
@@ -49,24 +57,11 @@ impl Block {
             position: Position::new(position.x, position.y, position.z),
         }
     }
-
-    fn is_visible(self, camera_pos: IVec3) -> bool {
-        let x = self.position.x;
-        let y = self.position.y;
-        let z = self.position.z;
-        let cx = camera_pos.x;
-        let cy = camera_pos.y;
-        let cz = camera_pos.z;
-        let x_diff = (x - cx).abs();
-        let y_diff = (y - cy).abs();
-        let z_diff = (z - cz).abs();
-        x_diff < VIEW_DISTANCE && y_diff < VIEW_DISTANCE && z_diff < VIEW_DISTANCE
-    }
 }
 
 impl Hash for Chunk {
     fn hash<H: Hasher>(&self, _state: &mut H) {
-        let mut hasher = DefaultHasher::new();
+        let mut hasher = FxHasher::default();
         self.center_pos.hash(&mut hasher);
         hasher.finish();
     }
@@ -109,191 +104,87 @@ impl Chunk {
 
         let blocks = blocks.lock().unwrap();
 
-        // let blocks = HashSet::new();
-                
-        // for x in 0..CHUNK_SIZE {
-        //     for z in 0..CHUNK_SIZE {
-        //         let noise = height_map.get_value(x as usize, z as usize);
-        //         let height = (noise * 10.0) as i32;
-        //         for y in 0..height {
-        //             let block = Block::new(IVec3::new(x, y, z));
-        //             blocks.insert(block);
-        //         }
-        //     }
-        // }
         Self {
             blocks: blocks.clone(),
             center_pos,
+            rendered: false,
         }
-    }
-
-    fn is_visible(&self, camera_pos: IVec3) -> bool {
-        let x = self.center_pos.x;
-        let z = self.center_pos.y;
-        let cx = camera_pos.x / CHUNK_SIZE;
-        let cz = camera_pos.z / CHUNK_SIZE;
-        let x_diff = (x - cx).abs();
-        let z_diff = (z - cz).abs();
-        x_diff < VIEW_DISTANCE && z_diff < VIEW_DISTANCE
     }
 }
 
 impl ChunkManager {  
-    pub fn update(&mut self, camera_pos: IVec3) {
-        let chunk_x = camera_pos.x / CHUNK_SIZE;
-        let chunk_z = camera_pos.z / CHUNK_SIZE;
-
-        // Remove chunks that are no longer visible.
-        self.visible_chunks.retain(|chunk| chunk.is_visible(camera_pos));
-
-        // If a chunk in the cache is now visible, move it to the visible chunks.
-        for chunk in self.chunk_cache.iter() {
-            if chunk.is_visible(camera_pos) {
-                self.visible_chunks.insert(chunk.clone());
-            }
+    pub fn update(&mut self, camera_transform: &Transform) {
+        let camera_pos = camera_transform.translation;
+        let far_pos = camera_transform.forward() * CHUNK_SIZE as f32 * VIEW_DISTANCE as f32;
+        let end_pos = camera_pos + far_pos;
+        if let Some(block) = raycast(camera_pos, end_pos, self) {
+            println!("Block found at {:?}", block.position);
+            let chunk_pos = IVec2::new(block.position.x / CHUNK_SIZE, block.position.z / CHUNK_SIZE);
+            let chunk = Chunk::new(chunk_pos);
+            self.add_visible_chunk(&chunk);
         }
-
-        self.chunk_cache.retain(|chunk| {
-            !self.visible_chunks.contains(chunk)
-        });
-        
-        // ----- Parallel version of the commented code. Not sure if it works properly yet. -----
-        let visible_chunks_mutex = Mutex::new(self.visible_chunks.clone());
-        (-GENERATION_RADIUS..GENERATION_RADIUS).into_par_iter().for_each(|x| {
-            (-GENERATION_RADIUS..GENERATION_RADIUS).into_par_iter().for_each(|z| {
-                if visible_chunks_mutex.lock().unwrap().contains(&Chunk::new(IVec2::new(chunk_x + x, chunk_z + z))) {
-                    return;
-                }
-                let chunk = Chunk::new(IVec2::new(chunk_x + x, chunk_z + z));
-                if chunk.is_visible(camera_pos) {
-                    visible_chunks_mutex.lock().unwrap().insert(chunk);
-                }
-            });
-        });
-        self.visible_chunks = visible_chunks_mutex.into_inner().unwrap();
     }
 
-    pub fn load_chunks(
-        &self,
-        commands: &mut Commands,
-        meshes: &mut ResMut<Assets<Mesh>>,
-        materials: &mut ResMut<Assets<StandardMaterial>>,
-        camera_pos: IVec3,
-    ) {
-        // ----- Parallel version of the commented code. Not sure if it works properly yet. -----
-        // Convert HashSet to Vec
-        let visible_chunks_vec: Vec<&Chunk> = self.visible_chunks.iter().collect();
+    fn add_visible_chunk(&mut self, chunk: &Chunk) {
+        if !self.visible_chunks.contains(&chunk) {
+            println!("Visible chunk added");
+            self.visible_chunks.insert(chunk.clone());
+            self.chunk_cache.insert(chunk.clone());
+        }
+    }
 
-        // Create a mutex for all mutable data
-        let shared_data = Arc::new(Mutex::new((commands, meshes, materials)));
-
-        // Parallelize loop over visible chunks
-        visible_chunks_vec.par_iter().for_each(|chunk| {
-            // Sequential loop over blocks in each chunk
+    pub fn get_block(&self, pos: IVec3) -> Option<&Block> {
+        for chunk in self.visible_chunks.iter() {
             for block in chunk.blocks.iter() {
-                if block.is_visible(camera_pos) {
-                    // Lock the mutex to access the mutable data
-                    let mut data = shared_data.lock().unwrap();
-                    let (commands, meshes, materials) = &mut *data;
-
-                    commands.spawn(PbrBundle {
-                        mesh: meshes.add(Mesh::from(shape::Cube { size: 0.5 })),
-                        material: materials.add(Color::rgb(0.5, 0.5, 1.0).into()),
-                        transform: Transform::from_translation(Vec3::new(
-                            block.position.x as f32,
-                            block.position.y as f32,
-                            block.position.z as f32,
-                        )),
-                        ..Default::default()
-                    });
+                if block.position == Position::from_ivec3(pos) {
+                    return Some(block);
                 }
             }
-        });
+        }
+        None
     }
 
-    pub fn unload_chunks(
-        &self,
-        commands: &mut Commands,
-        camera_pos: IVec3,
-        query: Query<Entity, With<Block>>,
-    ) {
-        for entity in query.iter() {
-            let block = query.get(entity).unwrap();
-            let object: &Block = query.get_component(entity).unwrap();
-            if !object.is_visible(camera_pos) {
-                commands.entity(block).despawn();
+    pub fn get_chunk(&self, pos: IVec2) -> Option<&Chunk> {
+        for chunk in self.visible_chunks.iter() {
+            if chunk.center_pos == pos {
+                return Some(chunk);
             }
         }
+        None
+    }
+
+    // Using the chunk data and the block positions, create the actual blocks on the screen
+    pub fn render_chunk(
+        &self,
+        commands: &mut Commands,
+        materials: &mut ResMut<Assets<StandardMaterial>>,
+        meshes: &mut ResMut<Assets<Mesh>>,
+        chunk: &Chunk,
+    ) {
+        if chunk.rendered {
+            return;
+        }
+
+        for block in chunk.blocks.iter() {
+            let block_pos = Vec3::new(
+                block.position.x as f32,
+                block.position.y as f32,
+                block.position.z as f32,
+            );
+            let block_transform = Transform::from_translation(block_pos);
+            commands
+                .spawn(PbrBundle {
+                    mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+                    material: materials.add(Color::rgb(0.5, 0.5, 1.0).into()),
+                    transform: block_transform,
+                    ..Default::default()
+                })
+                .insert(block.clone());
+        }
+
+        println!("Chunk rendered")
     }
 }
-
-// impl ChunkManager {  
-//     pub fn update(&mut self, camera_pos: IVec3) {
-//         let chunk_x = camera_pos.x / CHUNK_SIZE;
-//         let chunk_z = camera_pos.z / CHUNK_SIZE;
-//         let new_visible_chunks = Mutex::new(HashSet::new());
-//         let new_chunk_cache = HashSet::new();
-    
-//         let chunks = (0..GENERATION_RADIUS * 2)
-//             .flat_map(|x| (0..GENERATION_RADIUS * 2).map(move |z| (x - GENERATION_RADIUS, z - GENERATION_RADIUS)))
-//             .collect::<Vec<_>>();
-    
-//         chunks.par_iter().for_each(|(x, z)| {
-//             for chunk in self.visible_chunks.iter().chain(self.chunk_cache.iter()) {
-//                 if chunk.center_pos.x == chunk_x + x && chunk.center_pos.y == chunk_z + z {
-//                     return;
-//                 }
-//             }
-//             let chunk = Chunk::new(IVec2::new(chunk_x + x, chunk_z + z));
-//             new_visible_chunks.lock().unwrap().insert(chunk);
-//         });
-    
-//         self.visible_chunks.extend(new_visible_chunks.lock().unwrap().iter().cloned());
-//         self.chunk_cache.extend(new_chunk_cache);
-//     }
-
-//     pub fn load_chunks(
-//         &self,
-//         commands: &mut Commands,
-//         meshes: &mut ResMut<Assets<Mesh>>,
-//         materials: &mut ResMut<Assets<StandardMaterial>>,
-//         camera_pos: IVec3,
-//     ) {
-//         // MAKE THIS PARALLEL!!!
-//         // Render the visible blocks in the visible chunks
-//         for chunk in self.visible_chunks.iter() {
-//             for block in chunk.blocks.iter() {
-//                 if block.is_visible(camera_pos) {
-//                     commands.spawn(PbrBundle {
-//                         mesh: meshes.add(Mesh::from(shape::Cube { size: 0.5 })),
-//                         material: materials.add(Color::rgb(0.5, 0.5, 1.0).into()),
-//                         transform: Transform::from_translation(Vec3::new(
-//                             block.position.x as f32,
-//                             block.position.y as f32,
-//                             block.position.z as f32,
-//                         )),
-//                         ..Default::default()
-//                     });
-//                 }
-//             }
-//         }
-//     }
-
-//     pub fn unload_chunks(
-//         &self,
-//         commands: &mut Commands,
-//         camera_pos: IVec3,
-//         query: Query<Entity, With<Block>>,
-//     ) {
-//         for entity in query.iter() {
-//             let block = query.get(entity).unwrap();
-//             let object: &Block = query.get_component(entity).unwrap();
-//             if !object.is_visible(camera_pos) {
-//                 commands.entity(block).despawn();
-//             }
-//         }
-//     }
-// }
 
 pub fn init_world(
     mut commands: Commands,
@@ -301,8 +192,10 @@ pub fn init_world(
     mut meshes: ResMut<Assets<Mesh>>,
     mut chunk_manager: ResMut<ChunkManager>,
 ) {
-    chunk_manager.update(IVec3::new(0, 0, 0));
-    chunk_manager.load_chunks(&mut commands, &mut meshes, &mut materials, IVec3::new(1, 1, 1));
+    // Generate the initial chunks
+    let mut chunk = Chunk::new(IVec2::new(0, 0));
+    chunk_manager.add_visible_chunk(&chunk);
+    chunk_manager.render_chunk(&mut commands, &mut materials, &mut meshes, &chunk);
     println!("World initialized");
 }
 
@@ -312,19 +205,43 @@ pub fn update_world(
     mut meshes: ResMut<Assets<Mesh>>,
     mut chunk_manager: ResMut<ChunkManager>,
     camera: Query<&Transform, With<Camera3d>>,
-    entities: Query<Entity, With<Block>>,
 ) {
     // Get camera position
-    let pos = camera.single();
-    let pos = IVec3::new(
-        pos.translation.x.floor() as i32,
-        pos.translation.y.floor() as i32,
-        pos.translation.z.floor() as i32,
-    );
+    let transform = camera.single();
 
     // Update the chunk data
-    chunk_manager.update(pos);
-    chunk_manager.unload_chunks(&mut commands, pos, entities);
-    chunk_manager.load_chunks(&mut commands, &mut meshes, &mut materials, pos);
-    println!("World updated");
+    chunk_manager.update(transform);
+    chunk_manager.visible_chunks.iter().for_each(|chunk| {
+        chunk_manager.render_chunk(&mut commands, &mut materials, &mut meshes, chunk);
+        chunk.rendered = true;
+    });
+    // println!("World updated");
 }
+
+fn raycast(
+    start: Vec3,
+    end: Vec3,
+    chunk_manager: &ChunkManager,
+) -> Option<Block> {
+    let direction = (end - start).normalize();
+    let mut current_pos = start;
+    let mut distance = 0.0;
+    loop {
+        let block_pos = IVec3::new(
+            current_pos.x as i32,
+            current_pos.y as i32,
+            current_pos.z as i32,
+        );
+        let block = Block::new(block_pos);
+        if chunk_manager.get_block(block_pos).is_some() {
+            return Some(block);
+        }
+        current_pos += direction;
+        distance += 1.0;
+        if distance > CHUNK_SIZE as f32 {
+            break;
+        }
+    }
+    None
+}
+
