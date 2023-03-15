24ca94f6df6a3864eedb65e525cf73c25c40ce1f
Andrew Todd <andrew.s.todd@wsu.edu>
2023-03-15 14:52:47 -0700

    Formatted code.

--- src/world.rs
+++ src/world.rs
@@ -1,15 +1,15 @@
-use std::hash::{Hash};
-use std::sync::{Mutex, Arc};
+use bevy::prelude::*;
 use bevy::render::mesh::Indices;
-use bevy::render::render_resource::{PrimitiveTopology};
-use bevy::{prelude::*};
-use std::collections::{HashSet, HashMap};
-use noise::{Perlin, Fbm};
-use noise::utils::{NoiseMapBuilder, PlaneMapBuilder, NoiseMap};
+use bevy::render::render_resource::PrimitiveTopology;
+use noise::utils::{NoiseMap, NoiseMapBuilder, PlaneMapBuilder};
+use noise::{Fbm, Perlin};
 use rayon::prelude::*;
+use std::collections::{HashMap, HashSet};
+use std::hash::Hash;
+use std::sync::{Arc, Mutex};
 
 const CHUNK_SIZE: i32 = 16;
-const SEED : u32 = 69;
+const SEED: u32 = 69;
 
 // ---------- Block ----------
 #[derive(Component, Clone, Copy, PartialEq, Eq, Hash, Debug)]
@@ -19,9 +19,7 @@ struct Block {
 
 impl Block {
     fn new(position: IVec3) -> Self {
-        Self {
-            position: position,
-        }
+        Self { position }
     }
 }
 // --------------------------
@@ -44,7 +42,10 @@ impl Chunk {
     }
 
     fn gen_blocks(&mut self, noise: &NoiseMap) {
-        println!("Generating blocks for chunk at ({}, {})", self.start_pos.x, self.start_pos.y);
+        println!(
+            "Generating blocks for chunk at ({}, {})",
+            self.start_pos.x, self.start_pos.y
+        );
         for x in 0..CHUNK_SIZE {
             for z in 0..CHUNK_SIZE {
                 let height = noise.get_value(x as usize, z as usize) * 10.0;
@@ -59,22 +60,31 @@ impl Chunk {
         let mut verticies = Vec::new();
         let mut indicies = Vec::new();
 
-        let visible_blocks = self.blocks.par_iter().filter(|block| {
-            let block_pos = block.position;
-            let block_pos = Vec3::new(block_pos.x as f32, block_pos.y as f32, block_pos.z as f32);
-            let block_pos = block_pos + Vec3::new(0.5, 0.5, 0.5);
+        let visible_blocks = self
+            .blocks
+            .par_iter()
+            .filter(|block| {
+                let block_pos = block.position;
+                let block_pos =
+                    Vec3::new(block_pos.x as f32, block_pos.y as f32, block_pos.z as f32);
+                let block_pos = block_pos + Vec3::new(0.5, 0.5, 0.5);
 
-            // Check if the block is surrounded by other blocks
-            // If it is, don't render it
-            self.blocks.par_iter().any(|other_block| {
-                let other_block_pos = other_block.position;
-                let other_block_pos = Vec3::new(other_block_pos.x as f32, other_block_pos.y as f32, other_block_pos.z as f32);
-                let other_block_pos = other_block_pos + Vec3::new(0.5, 0.5, 0.5);
+                // Check if the block is surrounded by other blocks
+                // If it is, don't render it
+                self.blocks.par_iter().any(|other_block| {
+                    let other_block_pos = other_block.position;
+                    let other_block_pos = Vec3::new(
+                        other_block_pos.x as f32,
+                        other_block_pos.y as f32,
+                        other_block_pos.z as f32,
+                    );
+                    let other_block_pos = other_block_pos + Vec3::new(0.5, 0.5, 0.5);
 
-                let distance = block_pos.distance(other_block_pos);
-                distance > 1.0
+                    let distance = block_pos.distance(other_block_pos);
+                    distance > 1.0
+                })
             })
-        }).collect::<Vec<_>>();
+            .collect::<Vec<_>>();
 
         // For each visible block, get the verticies and indicies that are not back to back with other blocks.
         // This will result in a smaller mesh, and less draw calls.
@@ -87,7 +97,10 @@ impl Chunk {
             let mut block_indicies = Vec::new();
 
             // Front
-            if !self.blocks.contains(&Block::new(block_pos.as_ivec3() + IVec3::new(0, 0, 1))) {
+            if !self
+                .blocks
+                .contains(&Block::new(block_pos.as_ivec3() + IVec3::new(0, 0, 1)))
+            {
                 block_verticies.push(block_pos + Vec3::new(-0.5, -0.5, 0.5));
                 block_verticies.push(block_pos + Vec3::new(0.5, -0.5, 0.5));
                 block_verticies.push(block_pos + Vec3::new(0.5, 0.5, 0.5));
@@ -102,7 +115,10 @@ impl Chunk {
             }
 
             // Back
-            if !self.blocks.contains(&Block::new(block_pos.as_ivec3() + IVec3::new(0, 0, -1))) {
+            if !self
+                .blocks
+                .contains(&Block::new(block_pos.as_ivec3() + IVec3::new(0, 0, -1)))
+            {
                 block_verticies.push(block_pos + Vec3::new(-0.5, -0.5, -0.5));
                 block_verticies.push(block_pos + Vec3::new(0.5, -0.5, -0.5));
                 block_verticies.push(block_pos + Vec3::new(0.5, 0.5, -0.5));
@@ -117,7 +133,10 @@ impl Chunk {
             }
 
             // Left
-            if !self.blocks.contains(&Block::new(block_pos.as_ivec3() + IVec3::new(-1, 0, 0))) {
+            if !self
+                .blocks
+                .contains(&Block::new(block_pos.as_ivec3() + IVec3::new(-1, 0, 0)))
+            {
                 block_verticies.push(block_pos + Vec3::new(-0.5, -0.5, -0.5));
                 block_verticies.push(block_pos + Vec3::new(-0.5, -0.5, 0.5));
                 block_verticies.push(block_pos + Vec3::new(-0.5, 0.5, 0.5));
@@ -132,7 +151,10 @@ impl Chunk {
             }
 
             // Right
-            if !self.blocks.contains(&Block::new(block_pos.as_ivec3() + IVec3::new(1, 0, 0))) {
+            if !self
+                .blocks
+                .contains(&Block::new(block_pos.as_ivec3() + IVec3::new(1, 0, 0)))
+            {
                 block_verticies.push(block_pos + Vec3::new(0.5, -0.5, -0.5));
                 block_verticies.push(block_pos + Vec3::new(0.5, -0.5, 0.5));
                 block_verticies.push(block_pos + Vec3::new(0.5, 0.5, 0.5));
@@ -147,7 +169,10 @@ impl Chunk {
             }
 
             // Top
-            if !self.blocks.contains(&Block::new(block_pos.as_ivec3() + IVec3::new(0, 1, 0))) {
+            if !self
+                .blocks
+                .contains(&Block::new(block_pos.as_ivec3() + IVec3::new(0, 1, 0)))
+            {
                 block_verticies.push(block_pos + Vec3::new(-0.5, 0.5, -0.5));
                 block_verticies.push(block_pos + Vec3::new(0.5, 0.5, -0.5));
                 block_verticies.push(block_pos + Vec3::new(0.5, 0.5, 0.5));
@@ -162,7 +187,10 @@ impl Chunk {
             }
 
             // Bottom
-            if !self.blocks.contains(&Block::new(block_pos.as_ivec3() + IVec3::new(0, -1, 0))) {
+            if !self
+                .blocks
+                .contains(&Block::new(block_pos.as_ivec3() + IVec3::new(0, -1, 0)))
+            {
                 block_verticies.push(block_pos + Vec3::new(-0.5, -0.5, -0.5));
                 block_verticies.push(block_pos + Vec3::new(0.5, -0.5, -0.5));
                 block_verticies.push(block_pos + Vec3::new(0.5, -0.5, 0.5));
@@ -180,7 +208,8 @@ impl Chunk {
             indicies.extend(block_indicies);
         });
 
-        self.mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, verticies);
+        self.mesh
+            .insert_attribute(Mesh::ATTRIBUTE_POSITION, verticies);
         self.mesh.set_indices(Some(Indices::U32(indicies)));
     }
 }
@@ -199,10 +228,10 @@ impl FromWorld for Map {
         let fbm = Fbm::<Perlin>::new(SEED);
 
         let height_map = PlaneMapBuilder::<_, 2>::new(&fbm)
-        .set_size(1024, 1024)
-        .set_x_bounds(-5.0, 5.0)
-        .set_y_bounds(-5.0, 5.0)
-        .build();
+            .set_size(1024, 1024)
+            .set_x_bounds(-5.0, 5.0)
+            .set_y_bounds(-5.0, 5.0)
+            .build();
 
         Map {
             chunks: HashMap::new(),
@@ -215,16 +244,24 @@ impl FromWorld for Map {
 
 // ---------- Systems ----------
 pub fn initialize_world(mut commands: Commands, mut map: ResMut<Map>) {
-        // Generate four chunks.
-        for x in 0..2 {
-            for y in 0..2 {
-                let chunk_pos = IVec2::new(x * CHUNK_SIZE, y * CHUNK_SIZE);
-                let mut chunk = Chunk::new(chunk_pos);
-                chunk.gen_blocks(&map.noise);
-                chunk.gen_mesh();
-                map.chunks.insert(chunk_pos, chunk.clone());
-                commands.spawn(chunk);
-            }
+    // Generate four chunks.
+    for x in 0..4 {
+        for y in 0..4 {
+            let chunk_pos = IVec2::new(x * CHUNK_SIZE, y * CHUNK_SIZE);
+            let mut chunk = Chunk::new(chunk_pos);
+            chunk.gen_blocks(&map.noise);
+            chunk.gen_mesh();
+            map.chunks.insert(chunk_pos, chunk.clone());
+            commands.spawn(chunk);
         }
+    }
+
+    // Find the total blocks generated.
+    let mut total_blocks = 0;
+    map.chunks.iter().for_each(|(_, chunk)| {
+        total_blocks += chunk.blocks.len();
+    });
+
+    println!("Total blocks: {}", total_blocks);
 }
 // -----------------------------
