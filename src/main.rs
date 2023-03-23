470305ae21a6e44620107b8d34f87b83f5143a34
Andrew Todd <andrew.s.todd@wsu.edu>
2023-03-23 15:06:46 -0700

    Have part of a chunk working. There seems to be a disconnect between how many blocks it's creating and how many are showing up on my screen.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::sync::Arc;
+
 use bevy::{
     diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
     prelude::*,
@@ -13,20 +15,35 @@ use world::*;
 // property of the fly camera with "T"
 
 fn init(mut commands: Commands) {
-    commands.spawn(DirectionalLightBundle {
-        transform: Transform::from_translation(Vec3::new(10.0, 10.0, 10.0)),
-        ..Default::default()
+    // commands.spawn(PointLightBundle {
+    //     point_light: PointLight {
+    //         intensity: 1500.0,
+    //         shadows_enabled: true,
+    //         ..default()
+    //     },
+    //     transform: Transform::from_xyz(4.0, 8.0, 4.0),
+    //     ..default()
+    // });
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            intensity: 2500.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(10.0, 12.0, 10.0),
+        ..default()
     });
-    commands
-        .spawn(Camera3dBundle::default())
-        .insert(FlyCamera::default());
+    commands.spawn(Camera3dBundle {
+            transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+            ..default()
+        }).insert(FlyCamera::default());
 
     println!("Started Camera!");
 }
 
 fn main() {
     App::new()
-        // .insert_resource(Msaa::Sample4)
+        .insert_resource(Msaa::Sample4)
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
                 title: "Minecraft".to_string(),
--- src/world.rs
+++ src/world.rs
@@ -6,20 +6,25 @@ use noise::{Fbm, Perlin};
 use rayon::prelude::*;
 use std::collections::{HashMap, HashSet};
 use std::hash::Hash;
+use std::sync::Arc;
 
 const CHUNK_SIZE: i32 = 16;
 const SEED: u32 = 69;
 const BLOCK_SIZE: Vec3 = Vec3::new(1.0, 1.0, 1.0);
 
 // ---------- Block ----------
-#[derive(Component, Clone, Copy, PartialEq, Eq, Hash, Debug)]
+#[derive(Component, Clone, PartialEq, Eq, Hash, Debug)]
 struct Block {
     position: IVec3,
+    mesh: Handle<Mesh>,
 }
 
 impl Block {
     fn new(position: IVec3) -> Self {
-        Self { position }
+        Self {
+            position,
+            mesh: Default::default(),
+        }
     }
 
     fn get_pos(&self) -> IVec3 {
@@ -32,14 +37,12 @@ impl Block {
 #[derive(Component, Clone)]
 pub struct Chunk {
     blocks: HashSet<Block>,
-    mesh: Handle<Mesh>,
 }
 
 impl Chunk {
     fn new() -> Self {
         Self {
             blocks: HashSet::new(),
-            mesh: Default::default(),
         }
     }
 
@@ -47,7 +50,7 @@ impl Chunk {
         for x in 0..CHUNK_SIZE {
             for z in 0..CHUNK_SIZE {
                 let height = noise.get_value(x as usize, z as usize) * 10.0;
-                for y in 0..height as i32 {
+                for y in -2..height as i32 {
                     let block_pos = IVec3::new(x, y, z);
                     let block = Block::new(block_pos);
                     self.blocks.insert(block);
@@ -56,40 +59,32 @@ impl Chunk {
         }
     }
 
-    fn gen_mesh(&mut self) -> Mesh {
-        let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
-        let mut verticies = Vec::new();
-        let mut indicies = Vec::new();
-
+    fn gen_meshes(&mut self, meshes: &mut ResMut<Assets<Mesh>>) {
         let visible_blocks = self
             .blocks
             .par_iter()
-            .filter(|block| {
+            .filter(| block| {
                 let block_pos = block.position;
-                let block_pos =
-                    Vec3::new(block_pos.x as f32, block_pos.y as f32, block_pos.z as f32);
-                let block_pos = block_pos + BLOCK_SIZE;
 
                 // Check if the block is surrounded by other blocks
                 // If it is, don't render it
                 self.blocks.par_iter().any(|other_block| {
                     let other_block_pos = other_block.position;
-                    let other_block_pos = Vec3::new(
-                        other_block_pos.x as f32,
-                        other_block_pos.y as f32,
-                        other_block_pos.z as f32,
-                    );
-                    let other_block_pos = other_block_pos + BLOCK_SIZE;
 
-                    let distance = block_pos.distance(other_block_pos);
-                    distance > 1.0
+                    // Use the manhattan distance to check if the block is surrounded
+                    let distance = (block_pos.x - other_block_pos.x).abs() + (block_pos.y - other_block_pos.y).abs() + (block_pos.z - other_block_pos.z).abs();
+                    distance > 1
                 })
             })
             .collect::<Vec<_>>();
 
+        let mut new_meshes = HashMap::new();
+
         // For each visible block, get the verticies and indicies that are not back to back with other blocks.
         // This will result in a smaller mesh, and less draw calls.
         visible_blocks.iter().for_each(|block| {
+            let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
+
             let block_pos = block.position;
             let block_pos = Vec3::new(block_pos.x as f32, block_pos.y as f32, block_pos.z as f32);
             let block_pos = block_pos + BLOCK_SIZE;
@@ -140,17 +135,25 @@ impl Chunk {
             block_verticies.push(Vec3::new(block_pos.x + 1.0, block_pos.y - 1.0, block_pos.z + 1.0));
             block_verticies.push(Vec3::new(block_pos.x - 1.0, block_pos.y - 1.0, block_pos.z + 1.0));
 
-            verticies.extend(block_verticies);
-            indicies.extend(block_indicies);
+            mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, block_verticies);
+            mesh.set_indices(Some(Indices::U32(block_indicies)));
+            // In this example, normals and UVs don't matter,
+            // so we just use the same value for all of them
+            mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, vec![[0., 1., 0.]; 24]);
+            mesh.insert_attribute(Mesh::ATTRIBUTE_UV_0, vec![[0., 0.]; 24]);
+
+            new_meshes.insert(block.position, mesh);
         });
 
-        println!("verticies: {}", verticies.len());
-        println!("indicies: {}", indicies.len());
+        // Remove the blocks from self.blocks that are in new_meshes
+        self.blocks.retain(|block| !new_meshes.contains_key(&block.position));
 
-        mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, verticies);
-        mesh.set_indices(Some(Indices::U32(indicies)));
-
-        mesh
+        for (position, mesh) in new_meshes {
+            self.blocks.insert(Block {
+                position,
+                mesh: meshes.add(mesh),
+            });
+        }
     }
 }
 // ---------------------------
@@ -190,40 +193,29 @@ pub fn initialize_world(
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
     // Generate four chunks.
-    // for x in 0..4 {
-    //     for y in 0..4 {
-    //         let chunk_pos = IVec2::new(x * CHUNK_SIZE, y * CHUNK_SIZE);
-    //         println!("Generating chunk at {:?}", chunk_pos);
-    //         let mut chunk = Chunk::new();
-    //         chunk.gen_blocks(&map.noise);
-    //         chunk.mesh = meshes.add(chunk.gen_mesh());
-    //         map.chunks.insert(chunk_pos, chunk);
-    //     }
-    // }
+    for x in 0..4 {
+        for y in 0..4 {
+            let chunk_pos = IVec2::new(x * CHUNK_SIZE, y * CHUNK_SIZE);
+            println!("Generating chunk at {:?}", chunk_pos);
+            let mut chunk = Chunk::new();
+            chunk.gen_blocks(&map.noise);
+            chunk.gen_meshes(&mut meshes);
+            map.chunks.insert(chunk_pos, chunk);
+        }
+    }
 
     // Add the chunk to the world.
-    // for (chunk_pos, chunk) in map.chunks.iter() {
-    //     commands.spawn(PbrBundle {
-    //         mesh: chunk.mesh.clone(),
-    //         material: materials.add(Color::rgb(1.0, 0.0, 0.0).into()),
-    //         transform: Transform::from_translation(chunk_pos.as_vec2().extend(0.0)),
-    //         ..Default::default()
-    //     });
-    // }
-
-    // Generate a single chunk.
-    let chunk_pos = IVec2::new(0, 0);
-    let mut chunk = Chunk::new();
-    chunk.gen_blocks(&map.noise);
-    chunk.mesh = meshes.add(chunk.gen_mesh());
-    map.chunks.insert(chunk_pos, chunk);
-
-    commands.spawn(PbrBundle {
-        mesh: map.chunks.get(&chunk_pos).unwrap().mesh.clone(),
-        material: materials.add(Color::rgb(0.0, 1.0, 0.0).into()),
-        transform: Transform::from_translation(chunk_pos.as_vec2().extend(0.0)),
-        ..Default::default()
-    });
+    for (_chunk_pos, chunk) in map.chunks.iter() {
+        for block in chunk.blocks.iter() {
+            commands
+                .spawn(PbrBundle {
+                    mesh: block.mesh.clone(),
+                    material: materials.add(Color::rgb(0.0, 1.0, 0.0).into()),
+                    transform: Transform::from_translation(block.position.as_vec3()),
+                    ..Default::default()
+                });
+        }
+    }
 
     // Find the total blocks generated.
     let total_blocks = map
