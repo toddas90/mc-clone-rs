30fd57d6d7cd413931da4fe976f20aef2c726706
Andrew Todd <andrew.s.todd@wsu.edu>
2023-03-23 18:28:03 -0700

    AHHH I got something very usable! I can start tweaking performance!

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use std::sync::Arc;
+use std::{sync::Arc, f32::consts::PI};
 
 use bevy::{
     diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
     prelude::*,
-    window::{WindowMode, WindowResolution},
+    window::{WindowMode, WindowResolution}, pbr::{DirectionalLightShadowMap, CascadeShadowConfigBuilder},
 };
 use bevy_fly_camera::{FlyCamera, FlyCameraPlugin};
 
@@ -15,9 +15,29 @@ use world::*;
 // property of the fly camera with "T"
 
 fn init(mut commands: Commands) {
+    // directional 'sun' light
     commands.spawn(DirectionalLightBundle {
-        transform: Transform::from_xyz(10.0, 10.0, 10.0),
-        ..Default::default()
+        directional_light: DirectionalLight {
+            shadows_enabled: true,
+            illuminance: 5000.0,
+            ..default()
+        },
+        transform: Transform {
+            translation: Vec3::new(0.0, 2.0, 0.0),
+            rotation: Quat::from_rotation_x(-PI / 4.),
+            ..default()
+        },
+        // The default cascade config is designed to handle large scenes.
+        // As this example has a much smaller world, we can tighten the shadow
+        // bounds for better visual quality.
+        cascade_shadow_config: CascadeShadowConfigBuilder {
+            first_cascade_far_bound: 10.0,
+            num_cascades: 4,
+            maximum_distance: 100.0,
+            ..default()
+        }
+        .into(),
+        ..default()
     });
     // commands.spawn(PointLightBundle {
     //     point_light: PointLight {
@@ -52,6 +72,7 @@ fn main() {
         .add_plugin(FrameTimeDiagnosticsPlugin::default())
         .add_plugin(FlyCameraPlugin)
         .init_resource::<Map>()
+        .insert_resource(DirectionalLightShadowMap { size: 2048 })
         .add_startup_system(init)
         .add_startup_system(initialize_world)
         // .add_system(update_world)
