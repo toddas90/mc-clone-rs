da88da7cc29a36e5aca15281da2eef1eec289c1c
Andrew Todd <andrew.s.todd@wsu.edu>
2023-03-27 14:57:07 -0700

    SOME CHUNKS SPAWNED INNNNNN. Very low fps and it definitely wasn't working right, but it's progress.

--- src/world.rs
+++ src/world.rs
@@ -1,6 +1,7 @@
 use bevy::prelude::*;
 use bevy::render::mesh::Indices;
 use bevy::render::render_resource::PrimitiveTopology;
+use bevy_flycam::FlyCam;
 use noise::utils::{NoiseMap, NoiseMapBuilder, PlaneMapBuilder};
 use noise::{Fbm, Perlin};
 use rayon::prelude::*;
@@ -360,76 +361,76 @@ pub fn update_world(
     mut map: ResMut<Map>,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
-    camera: Query<&Transform, With<Camera3d>>,
+    camera: Query<&Transform, With<FlyCam>>,
     entities: Query<(Entity, &Chunk), With<Chunk>>,
 ) {
     // In here, I will use the camera's position to determine which chunks to load and unload.
     let camera = camera.single();
     let pos = Vec2::new(camera.translation.x, camera.translation.z);
 
-    // let mut temp_cache = HashMap::new();
+    let mut temp_cache = HashMap::new();
 
-    // // If the chunk is outside of the render distance, unload it.
-    // map.chunks.iter().for_each(|(chunk_pos, chunk)| {
-    //     if pos.x - chunk_pos.x as f32 > (CHUNK_SIZE * RENDER_DISTANCE) as f32
-    //         || pos.y - chunk_pos.y as f32 > (CHUNK_SIZE * RENDER_DISTANCE) as f32
-    //     {
-    //         temp_cache.insert(chunk_pos.clone(), chunk.clone());
-    //     }
-    // });
+    // If the chunk is outside of the render distance, unload it.
+    map.chunks.iter().for_each(|(chunk_pos, chunk)| {
+        if pos.x - chunk_pos.x as f32 > (CHUNK_SIZE * RENDER_DISTANCE) as f32
+            || pos.y - chunk_pos.y as f32 > (CHUNK_SIZE * RENDER_DISTANCE) as f32
+        {
+            temp_cache.insert(chunk_pos.clone(), chunk.clone());
+        }
+    });
 
-    // // Unload the chunks.
-    // map.chunks
-    //     .retain(|chunk_pos, _| temp_cache.contains_key(chunk_pos) == false);
+    // Unload the chunks.
+    map.chunks
+        .retain(|chunk_pos, _| temp_cache.contains_key(chunk_pos) == false);
 
-    // // Put the chunks into the cache.
-    // if map.cache.len() < 17 {
-    //     map.cache.extend(temp_cache);
-    // } else {
-    //     // Remove chunks from cache.
-    //     let mut temp_cache = temp_cache;
-    //     map.cache.retain(|chunk_pos, _| {
-    //         if temp_cache.contains_key(chunk_pos) {
-    //             temp_cache.remove(chunk_pos);
-    //             return false;
-    //         }
-    //         return true;
-    //     });
+    // Put the chunks into the cache.
+    if map.cache.len() < 17 {
+        map.cache.extend(temp_cache);
+    } else {
+        // Remove chunks from cache.
+        let mut temp_cache = temp_cache;
+        map.cache.retain(|chunk_pos, _| {
+            if temp_cache.contains_key(chunk_pos) {
+                temp_cache.remove(chunk_pos);
+                return false;
+            }
+            return true;
+        });
 
-    //     // Add the new chunks to the cache.
-    //     map.cache.extend(temp_cache);
-    // }
+        // Add the new chunks to the cache.
+        map.cache.extend(temp_cache);
+    }
 
-    // // Despawn the chunks.
-    // for (entity, chunk) in entities.iter() {
-    //     if map.chunks.contains_key(&chunk.pos) == false {
-    //         println!("Despawning chunk at {:?}", chunk.pos);
-    //         commands.entity(entity).despawn_recursive();
-    //     }
-    // }
+    // Despawn the chunks.
+    for (entity, chunk) in entities.iter() {
+        if map.chunks.contains_key(&chunk.pos) == false {
+            println!("Despawning chunk at {:?}", chunk.pos);
+            commands.entity(entity).despawn_recursive();
+        }
+    }
 
-    // // Load the chunks.
-    // if map.chunks.len() < 10 as usize {
-    //     let chunk_pos = IVec2::new(
-    //         (pos.x / CHUNK_SIZE as f32).floor() as i32 * CHUNK_SIZE,
-    //         (pos.y / CHUNK_SIZE as f32).floor() as i32 * CHUNK_SIZE,
-    //     );
+    // Load the chunks.
+    if map.chunks.len() < 10 as usize {
+        let chunk_pos = IVec2::new(
+            (pos.x / CHUNK_SIZE as f32).floor() as i32 * CHUNK_SIZE,
+            (pos.y / CHUNK_SIZE as f32).floor() as i32 * CHUNK_SIZE,
+        );
 
-    //     if map.chunks.contains_key(&chunk_pos) == false {
-    //         if map.cache.contains_key(&chunk_pos) {
-    //             println!("Loading chunk at {:?} from cache", chunk_pos);
-    //             let chunk = map.cache.get(&chunk_pos).unwrap().clone();
-    //             map.chunks.insert(chunk_pos, chunk);
-    //         } else {
-    //             println!("Generating chunk at {:?}", chunk_pos);
-    //             let mut chunk = Chunk::new(chunk_pos);
-    //             chunk.gen_blocks(&map.noise);
-    //             chunk.gen_meshes(&mut meshes);
-    //             map.chunks.insert(chunk_pos, chunk);
-    //         }
-    //         spawn_chunks(commands, &map, meshes, materials);
-    //     }
-    // }
+        if map.chunks.contains_key(&chunk_pos) == false {
+            if map.cache.contains_key(&chunk_pos) {
+                println!("Loading chunk at {:?} from cache", chunk_pos);
+                let chunk = map.cache.get(&chunk_pos).unwrap().clone();
+                map.chunks.insert(chunk_pos, chunk);
+            } else {
+                println!("Generating chunk at {:?}", chunk_pos);
+                let mut chunk = Chunk::new(chunk_pos);
+                chunk.gen_blocks(&map.noise);
+                chunk.gen_meshes(&mut meshes);
+                map.chunks.insert(chunk_pos, chunk);
+            }
+            spawn_chunks(commands, &map, materials);
+        }
+    }
 }
 
 fn spawn_chunks(
